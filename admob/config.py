f3fd0df220c5d6651d2abfee07daad57dc24441c
arrkiin <arrkiin@gmail.com>
2019-07-06 16:57:07 +0200

    Fixed compilation problem, file not found error

--- admob/config.py
+++ admob/config.py
@@ -7,7 +7,8 @@ def configure(env):
 		env.android_add_java_dir("android")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
 		env.disable_module()
-            
+			
 	if env['platform'] == "iphone":
-		env.Append(FRAMEWORKPATH=['modules/admob/ios/lib'])
+		env.AppendUnique(FRAMEWORKPATH='#modules/admob/ios/lib')
+		env.AppendUnique(FRAMEWORKS=['-framework AdSupport', '-framework CoreTelephony', '-framework EventKit', '-framework EventKitUI', '-framework MessageUI', '-framework StoreKit', '-framework SafariServices', '-framework CoreBluetooth', '-framework AssetsLibrary', '-framework CoreData', '-framework CoreLocation', '-framework CoreText', '-framework ImageIO', '-framework GLKit', '-framework CoreVideo', '-framework CFNetwork', '-framework MobileCoreServices', '-framework GoogleMobileAds'])
 		env.Append(LINKFLAGS=['-ObjC', '-framework','AdSupport', '-framework','CoreTelephony', '-framework','EventKit', '-framework','EventKitUI', '-framework','MessageUI', '-framework','StoreKit', '-framework','SafariServices', '-framework','CoreBluetooth', '-framework','AssetsLibrary', '-framework','CoreData', '-framework','CoreLocation', '-framework','CoreText', '-framework','ImageIO', '-framework', 'GLKit', '-framework','CoreVideo', '-framework', 'CFNetwork', '-framework', 'MobileCoreServices', '-framework', 'GoogleMobileAds'])
